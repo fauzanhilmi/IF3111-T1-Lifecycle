c018353581916067d3273ad661113695d4983731
fauzanhilmi <fauzan_hilmi@students.itb.ac.id>
2015-02-08 14:42:17 +0700

    add broadcastreceiver

---
+++ BroadcastReceiverApp/app/src/main/java/phon/receiver/android/vogella/de/broadcastreceiverapp/MyPhoneReceiver.java
@@ -0,0 +1,29 @@
+package phon.receiver.android.vogella.de.broadcastreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+/**
+ * Created by USER on 2/8/2015.
+ */
+public class MyPhoneReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        if(extras!=null) {
+            String state = extras.getString(TelephonyManager.EXTRA_STATE);
+            Log.w("MY_DEBUG_TAG",state);
+            if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                String phoneNumber = extras
+                        .getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.w("MY_DEBUG_TAG",phoneNumber);
+            }
+        }
+    }
+
+}
